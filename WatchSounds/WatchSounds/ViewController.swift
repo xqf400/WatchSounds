9cb20d351aa6d185dbead98e903dde790acab0e0
Fabian Kuschk <8header8@googlemail.com>
2022-08-10 14:22:10 +0200

    init xcode project

---
+++ WatchSounds/WatchSounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchSounds
+//
+//  Created by Fabian Kuschke on 10.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
